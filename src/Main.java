0a56658c8d8cb977254bb39d13c53224c5fca493
gabdev12 <gabu.lopez12@gmail.com>
2022-06-20 21:22:49 -0500

    Funcion con tres parametros

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int resultado;
+        resultado = suma(10,10,10);
+
+        System.out.println(resultado);
+}
+    public static int suma(int a, int b, int c) {
+        return a + b + c;
+    }
+}
